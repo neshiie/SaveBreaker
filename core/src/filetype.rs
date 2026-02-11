6b839f912f83a71dcea4873230dfcbe28beda9c9
Chase Dillard <c.dillard@pm.me>
2026-02-11 09:28:28 -0500

    Fix clippy

--- core/src/filetype.rs
+++ core/src/filetype.rs
@@ -57,30 +57,14 @@ fn generate_file_format(filename: &str, buf: &[u8; 64]) -> FileFormat {
         },
     };
 
-    if opt.is_some() {
-        return opt.unwrap();
+    if let Some(v) = opt {
+        return v;
     }
 
     match buf {
-        [
-            0x53,
-            0x51,
-            0x4C,
-            0x69,
-            0x74,
-            0x65,
-            0x20,
-            0x66,
-            0x6F,
-            0x72,
-            0x6D,
-            0x61,
-            0x74,
-            0x20,
-            0x33,
-            0x00,
-            ..,
-        ] => FileFormat::Binary(BinarySignature::Sqlite),
+        [0x53, 0x51, 0x4C, 0x69, 0x74, 0x65, 0x20, 0x66, 0x6F, 0x72, 0x6D, 0x61, 0x74, 0x20, 0x33, 0x00, ..] => {
+            FileFormat::Binary(BinarySignature::Sqlite)
+        }
         _ => FileFormat::Binary(BinarySignature::Raw),
     }
 }
